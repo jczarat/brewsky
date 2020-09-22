4e08aad128c5562e4afdcc0cf4112e82c4a3ec7f
Jesse Zarat <jczarat@gmail.com>
2020-09-21 19:46:16 -0600

    add detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('breweries/', views.breweries_index, name='index'),
-    # path('breweries/<int:brewery_id>/', views.breweries_detail, name='detail'),
+    path('breweries/<str:brewery_id>/', views.breweries_detail, name='detail'),
     path('accounts/signup/', views.signup, name='signup'),
 ]
