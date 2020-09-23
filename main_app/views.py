60101784f2a75c63d76b89e608103026439b572e
James Garcia <jamesgarcia@Jamess-MacBook-Air.local>
2020-09-23 15:09:59 -0700

    working on pagination

--- main_app/views.py
+++ main_app/views.py
@@ -40,9 +40,12 @@ def breweries_index(request):
     by_state = None
     by_city = None
     by_postal = None
+    pnum = 1
+    if request.POST:
+        pnum = pnum + 1
     if 'state' in request.GET:
         state = request.GET['state']
-        s = requests.get(f'https://api.openbrewerydb.org/breweries?by_state={state}&per_page=50')
+        s = requests.get(f'https://api.openbrewerydb.org/breweries?by_state={state}&per_page=50&page={pnum}')
         by_state = s.json()
     if 'city' in request.GET:
         city = request.GET['city']
@@ -52,7 +55,7 @@ def breweries_index(request):
         postal = request.GET['postal']
         p = requests.get(f'https://api.openbrewerydb.org/breweries?by_postal={postal}&per_page=50')
         by_postal = p.json()
-    return render(request, 'breweries/index.html', {'breweries': breweries, 'by_state': by_state, 'by_city': by_city , 'by_postal': by_postal})
+    return render(request, 'breweries/index.html', {'breweries': breweries, 'by_state': by_state, 'by_city': by_city , 'by_postal': by_postal, "pnum": pnum})
 
 
 def breweries_detail(request, brewery_id):
